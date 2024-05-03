332819a5f17f45a0aaa4d9bf8e83c1967dd69f94
KanberZeren <zeren.kanber@student.htw-berlin.de>
2024-05-03 15:44:12 +0200

    Spring boot files

---
+++ src/main/java/ticketapp/webtech/ticketapp/TicketappApplication.java
@@ -0,0 +1,13 @@
+package ticketapp.webtech.ticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketappApplication.class, args);
+	}
+
+}
